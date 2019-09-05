f5600e643e6fe247cc79ec3d5d94e49e809b9ec3
thjxs <tanhongjianx@163.com>
2019-09-05 15:38:19 +0800

    Add jshint husky lint-staged

--- class.js
+++ class.js
@@ -1,15 +1,9 @@
 class Car{
     constructor() {
-      this.arr = []
+      this.arr = [];
     }
 }
 
 Car.prototype.condition = [];
 
-const a = new Car()
-a.condition.push('test')
-a.arr.push('1')
-const b = new Car()
-b.condition.push('???')
-b.arr.push('2')
-console.log(a.condition, a.arr, b.arr, b.condition);
+module.exports = Car;
