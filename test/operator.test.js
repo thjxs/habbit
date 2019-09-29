bdb39ee5aa038d6b3aab96a57358796c06862f17
thjxs <tanhongjianx@163.com>
2019-09-29 15:13:51 +0800

    Add operator in

--- test/operator.test.js
+++ test/operator.test.js
@@ -1,3 +1,6 @@
+/**
+ * JavaScript global variables are properties of the global object.
+ */
 const assert = require('assert');
 var globalVar = 'test';
 let globalLet = 'test';
@@ -6,15 +9,26 @@ this.varWithThis = 'test';
 let deleteOnthis = delete this.varWithThis;
 describe('# delete', function () {
   it('# global declare with var', function () {
-    assert.ok(!delete globalVar);
+    assert.ok(!delete globalVar); // jshint ignore:line
   });
   it('# global declare with let', function () {
-    assert.ok(!delete globalLet);
+    assert.ok(!delete globalLet); // jshint ignore:line
   });
   it('# global a deletable property of the global object', function () {
-    assert.ok(delete aVar);
+    assert.ok(delete aVar); // jshint ignore:line
   });
   it('# global declare with this', function () {
     assert.ok(deleteOnthis);
+  });
+});
+
+describe('# in', function() {
+  it('# in copy the names of all object properties into an array', function () {
+    const o = {x: 1, y: 2, z: 3};
+    const a = [];
+    const testArr = ['x', 'y', 'z'];
+    let i = 0;
+    for (a[i++] in o);
+    assert.deepStrictEqual(a, testArr);
   });
 });
