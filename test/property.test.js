655b2103dea0333713bd8b09e5c56c3c451578f5
thjxs <tanhongjianx@163.com>
2019-10-14 17:15:46 +0800

    Add property test

---
+++ test/property.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+describe('#property', function () {
+  var o = {};
+  var property = {
+    value: 1,
+    writable: true,
+    enumerable: false,
+    configurable: true
+  };
+  it('#define', () => {
+    Object.defineProperty(o, 'x', property);
+    assert.strictEqual(o.x, property.value);
+  });
+  it('#enumerable', () => {
+    assert.strictEqual(Object.keys(o).length, 0);
+  });
+  it('#writable false', () => {
+    Object.defineProperty(o, 'x', {writable: false});
+    let newValue = 2;
+    o.x = newValue;
+    assert.notStrictEqual(o.x, newValue);
+  });
+  it('#configurable', () => {
+    let newValue = 2;
+    Object.defineProperty(o, 'x', {value: newValue});
+    assert.strictEqual(o.x, newValue);
+  });
+});
