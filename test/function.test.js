ba8c4ec0a0cd82e6e8f32848db5f1dd65a8d991e
thjxs <tanhongjianx@163.com>
2019-10-21 11:51:23 +0800

    Add function test

---
+++ test/function.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+function test(p) {
+  return p;
+}
+
+let defaultValue = 12;
+
+function testParametersWithDefaultValue(p) {
+  p = p || defaultValue;
+  return p;
+}
+
+describe('#function paramaters', function () {
+  it('#input p', function () {
+    assert.strictEqual(test(1), 1);
+  });
+  it('#test parameters with default value', function () {
+    assert.strictEqual(testParametersWithDefaultValue(), defaultValue);
+  });
+});
