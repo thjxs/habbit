8e29d89a8cdcc77ab15738f8862c684779031ac5
thjxs <tanhongjianx@163.com>
2019-10-30 13:55:07 +0800

    Add deepmerge

---
+++ deepmerge.js
@@ -0,0 +1,23 @@
+function isObject(item) {
+  return item && typeof item === 'object' && !Array.isArray(item);
+}
+
+module.exports = function deepmerge(target, source, options = {clone: true}) {
+  const output = options.clone ? {...target} : target;
+
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).forEach(key => {
+      if (key === '__proto__') {
+        return;
+      }
+
+      if (isObject(source[key]) && key in target) {
+        output[key] = deepmerge(target[key], source[key], options);
+      } else {
+        output[key] = source[key];
+      }
+    });
+  }
+
+  return output;
+};
---
+++ test/deepmerge.test.js
@@ -0,0 +1,11 @@
+const deepmerge = require('../deepmerge');
+const assert = require('assert');
+
+describe('#deepmerge', () => {
+  it('should not be subject to prototype pollution', () => {
+    deepmerge({}, JSON.parse('{"myProperty": "a", "__proto__": {"isAdmin": true}}'), {
+      clone: false
+    });
+    assert.strictEqual({}.isAdmin, undefined);
+  });
+});
