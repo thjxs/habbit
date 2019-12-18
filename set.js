55b1309578d465e2aa26551cfc03e0511e406a91
thjxs <tanhongjianx@163.com>
2019-12-18 15:59:51 +0800

    Add Set

--- set.js
+++ set.js
@@ -1,7 +1,52 @@
-const set1 = new Set([1, 5])
-const o = {a: 1, b: 2}
-set1.add(o)
-set1.add({a: 1, b: 2})
-console.log(set1)
-o.a = 'yellow'
-console.log(set1)
+function isSuperset(set, subset) {
+  for (let elem of subset) {
+    if (!set.has(elem)) return false;
+  }
+  return true;
+}
+
+function union(setA, setB) {
+  let _union = new Set(setA);
+  for (let elem of setB) {
+    _union.add(elem);
+  }
+  return _union;
+}
+
+function intersection(setA, setB) {
+  let _intersection = new Set();
+  for (let elem of setB) {
+    if (setA.has(elem)) {
+      _intersection.add(elem);
+    }
+  }
+  return _intersection;
+}
+
+function symmetricDifference(setA, setB) {
+  let _difference = new Set(setA);
+  for (let elem of setB) {
+    if (_difference.has(elem)) {
+      _difference.delete(elem);
+    } else {
+      _difference.add(elem);
+    }
+  }
+  return _difference;
+}
+
+function difference(setA, setB) {
+  let _difference = new Set(setA);
+  for (let elem of setB) {
+    _difference.delete(elem);
+  }
+  return _difference;
+}
+
+module.exports = {
+  isSuperset,
+  union,
+  intersection,
+  symmetricDifference,
+  difference
+};
