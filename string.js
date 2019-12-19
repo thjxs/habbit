a0ed863ee50bacf149f498370a7442557dbbcb01
thjxs <tanhongjianx@163.com>
2019-12-19 16:17:52 +0800

    Add get words

--- string.js
+++ string.js
@@ -1,21 +1,21 @@
-function firstUniqChar (s) {
-  let sa = s.split('');
+function firstUniqChar(s) {
+  let sa = s.split("");
   let index;
   let value;
   let tmp = [];
-  for(let i = 0; i < sa.length; i += 1) {
+  for (let i = 0; i < sa.length; i += 1) {
     if (tmp[sa[i]] == undefined) {
       tmp[sa[i]] = 0;
     }
     tmp[sa[i]]++;
   }
-  for(let key in tmp) {
+  for (let key in tmp) {
     if (tmp[key] == 1) {
       value = key;
       break;
     }
   }
-  for(let i = 0; i < sa.length; i += 1) {
+  for (let i = 0; i < sa.length; i += 1) {
     if (sa[i] == value) {
       index = i;
     }
@@ -23,16 +23,16 @@ function firstUniqChar (s) {
   return index != undefined ? index : -1;
 }
 
-function matchParentheses (str) {
+function matchParentheses(str) {
   const stack = [];
   const mapper = {
-    ')': '(',
-    ']': '[',
-    '}': '{'
+    ")": "(",
+    "]": "[",
+    "}": "{"
   };
-  const leftParentheses = ['(', '[', '{'];
+  const leftParentheses = ["(", "[", "{"];
 
-  for(let i in str) {
+  for (let i in str) {
     if (leftParentheses.indexOf(str[i]) > -1) {
       stack.push(str[i]);
     } else {
@@ -48,12 +48,26 @@ function matchParentheses (str) {
   return true;
 }
 
-function reverseString (str) {
-  return str.split('').reverse().join('');
+function reverseString(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+/**
+ * get words
+ * trim whitespaces
+ * @param  {string} str
+ * @return {array}
+ */
+function getWords(str) {
+  return str.trim().split(/\s+/g);
 }
 
 module.exports = {
   firstUniqChar,
   matchParentheses,
-  reverseString
+  reverseString,
+  getWords
 };
--- test/array.test.js
+++ test/array.test.js
@@ -1,57 +1,66 @@
-const arr = require('../array');
-const assert = require('assert');
+const arr = require("../array");
+const assert = require("assert");
 
-describe('intersection', () => {
-  it('1', () => {
-    assert.deepStrictEqual(arr.intersect([1], [1,2,3]), [1]);
+describe("intersection", () => {
+  it("1", () => {
+    assert.deepStrictEqual(arr.intersect([1], [1, 2, 3]), [1]);
   });
-  it('1223, 23242', () => {
-    assert.deepStrictEqual(arr.intersect([1,2,2,3], [2,3,2,4,2]), [2,3,2]);
+  it("1223, 23242", () => {
+    assert.deepStrictEqual(arr.intersect([1, 2, 2, 3], [2, 3, 2, 4, 2]), [
+      2,
+      3,
+      2
+    ]);
   });
-  it('12, 34', () => {
-    assert.deepStrictEqual(arr.intersect([1,2], [3,4]), []);
+  it("12, 34", () => {
+    assert.deepStrictEqual(arr.intersect([1, 2], [3, 4]), []);
   });
 });
 
-describe('intersection v1', () => {
-  it('1', () => {
+describe("intersection v1", () => {
+  it("1", () => {
     assert.deepStrictEqual(arr.intersect_1([1], [1, 2, 3]), [1]);
   });
-  it('1223, 23242', () => {
-    assert.deepStrictEqual(arr.intersect_1([1,2,2,3], [2,3,2,4,2]), [2,3,2]);
+  it("1223, 23242", () => {
+    assert.deepStrictEqual(arr.intersect_1([1, 2, 2, 3], [2, 3, 2, 4, 2]), [
+      2,
+      3,
+      2,
+      2
+    ]);
   });
-  it('12, 34', () => {
-    assert.deepStrictEqual(arr.intersect_1([1,2], [3,4]), []);
+  it("12, 34", () => {
+    assert.deepStrictEqual(arr.intersect_1([1, 2], [3, 4]), []);
   });
 });
 
-describe('move zeros', () => {
-  it('1010', () => {
-    let a = [1,0,1,0];
+describe("move zeros", () => {
+  it("1010", () => {
+    let a = [1, 0, 1, 0];
     arr.moveZeroes(a);
-    assert.deepStrictEqual(a, [1,1,0,0]);
+    assert.deepStrictEqual(a, [1, 1, 0, 0]);
   });
-  it('11', () => {
-    let a = [1,1];
+  it("11", () => {
+    let a = [1, 1];
     arr.moveZeroes(a);
-    assert.deepStrictEqual(a, [1,1]);
+    assert.deepStrictEqual(a, [1, 1]);
   });
-  it('00', () => {
-    let a = [0,0];
+  it("00", () => {
+    let a = [0, 0];
     arr.moveZeroes(a);
     assert.deepStrictEqual(a, [0, 0]);
   });
 });
 
-describe('remove duplicates', () => {
-  it('1,1,2', () => {
-    const a = [1,1,2];
-    const carr = [1,2];
+describe("remove duplicates", () => {
+  it("1,1,2", () => {
+    const a = [1, 1, 2];
+    const carr = [1, 2];
     const index = arr.removeDuplicatezs(a);
     assert.strictEqual(index, carr.length);
     assert.deepStrictEqual(a.slice(0, index), carr);
   });
-  it('1,2,1,2', () => {
-    assert.strictEqual(arr.removeDuplicatezs([1,1,1,2,2,3,4]), 4);
+  it("1,2,1,2", () => {
+    assert.strictEqual(arr.removeDuplicatezs([1, 1, 1, 2, 2, 3, 4]), 4);
   });
 });
