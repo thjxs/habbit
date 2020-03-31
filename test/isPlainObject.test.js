ef3d52529d9092f702078d470627f337f8a57dea
thjxs <tanhongjianx@163.com>
2020-03-31 11:52:51 +0800

    Add isPlainObject

---
+++ test/isPlainObject.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const isPlainObject = require('../isPlainObject');
+
+describe('# is plain object', function () {
+  it('new Date()', function () {
+    assert.strictEqual(isPlainObject(new Date()), false);
+  });
+  it('new Test()', function () {
+    function Test() {
+      this.hi = 1;
+    }
+    assert.strictEqual(isPlainObject(new Test()), false);
+  });
+  it('{x: 1}', function () {
+    assert.strictEqual(isPlainObject({ x: 1 }), true);
+  });
+});
