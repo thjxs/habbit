706eabd28a7ab05ab2574aae910955d71f7121a7
thjxs <tanhongjianx@163.com>
2020-04-07 16:45:28 +0800

    Update async race test case

--- test/race.test.js
+++ test/race.test.js
@@ -1,21 +1,21 @@
 const assert = require('assert');
-it('#async', done => {
-  Promise.race([Promise.resolve(3), Promise.reject(5)]).then(value => {
+it('#async', (done) => {
+  Promise.race([Promise.resolve(3), Promise.reject(5)]).then((value) => {
     assert.strictEqual(value, 3);
     done();
   });
 });
 
-function delay(seconds) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve(seconds);
-    }, seconds * 1000);
+function delay(ms) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(ms);
+    }, ms);
   });
 }
 
-it('#async 6 3', done => {
-  Promise.race([delay(3), delay(1)]).then(value => {
+it('#async 6 3', (done) => {
+  Promise.race([delay(3), delay(1)]).then((value) => {
     assert.strictEqual(value, 1);
     done();
   });
